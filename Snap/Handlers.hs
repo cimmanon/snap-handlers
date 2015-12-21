61837a389348ec8178dc1fc3f591c547d272c301
cimmanon <chris@cimmanon.org>
2015-12-20 22:18:36 -0500

    Added missing file

---
+++ Snap/Handlers.hs
@@ -0,0 +1,9 @@
+module Snap.Handlers
+	( module Snap.Handlers.Digestive
+	, module Snap.Handlers.Errors
+	, module Snap.Handlers.Param
+	) where
+
+import Snap.Handlers.Digestive
+import Snap.Handlers.Errors
+import Snap.Handlers.Param
